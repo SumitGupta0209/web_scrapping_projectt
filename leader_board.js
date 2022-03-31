6de45c8777d25e8c35d5989059d968ff872b7111
SumitGupta0209 <53830900+SumitGupta0209@users.noreply.github.com>
2022-03-31 23:45:12 +0530

    from here we will get all the required detials of each batsman

--- leader_board.js
+++ leader_board.js
@@ -20,13 +20,43 @@ function cb(error,response,html)
 
         const document=dom.window.document;
 
-        let allScorecardtags=document.querySelectorAll('a[data-hover="Scorecard"]');
+        let allScorecardtags=document.querySelectorAll('a[data-hover="Scorecard"]');// array of scorecard
 
         for(let i=0;i<allScorecardtags.length;i++)
         {
             let link=allScorecardtags[i].href;
-            let completeLink="https://www.espncricinfo.com"+link;
-            console.log(completeLink);
+            let completeLink="https://www.espncricinfo.com"+link; //got the links every match scorecard
+            request(completeLink,cb2);
         }
+    }
+}
+
+function cb2(response,error,html)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    else
+    {
+        const dom=new JSDOM(html);
+        const document=dom.window.document;
+
+        let batsmanRows=document.querySelectorAll(".table.batsman tbody tr");
+
+        for(let i=0;i<batsmanRows.length;i++)
+        {
+            let cells=batsmanRows[i].querySelectorAll("td");
+            if(cells.length==8)
+            {
+                let name=cells[0].textContent;                  //from here we will get all the required detials of each batsman 
+                let runs=cells[2].textContent;
+                let balls=cells[3].textContent;
+                let fours=cells[5].textContent;
+                let six=cells[6].textContent;
+            }
+        }
+
+
     }
 }
