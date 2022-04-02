f918711a36dc1827d60949a4df10c73f572289b8
SumitGupta0209 <53830900+SumitGupta0209@users.noreply.github.com>
2022-04-02 23:30:37 +0530

    done now

--- leader_board.js
+++ leader_board.js
@@ -1,65 +1,60 @@
-const request=require("request");
+const request = require("request");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const fs = require("fs");
 
-const jsdom=require("jsdom");
-
-const{JSDOM}=jsdom;
-
-const link="https://www.espncricinfo.com/series/ipl-2021-1249214/match-results";
+const link = "https://www.espncricinfo.com/series/ipl-2021-1249214/match-results";
 
 let leaderboard = [];
+let counter = 0;
 
 request(link,cb);
 
-function cb(error,response,html)
-{
-    if(error)
-    {
+function cb(error,response,html){
+    if(error){
         console.log(error);
-    }
-    else
-    {
-        const dom=new JSDOM(html);
-
-        const document=dom.window.document;
-
-        let allScorecardtags=document.querySelectorAll('a[data-hover="Scorecard"]');// array of scorecard
-
-        for(let i=0;i<allScorecardtags.length;i++)
-        {
-            let link=allScorecardtags[i].href;
-            let completeLink="https://www.espncricinfo.com"+link; //got the links every match scorecard
+    }else{
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        let allScorecardTags = document.querySelectorAll('a[data-hover="Scorecard"]');
+        for(let i=0;i<allScorecardTags.length;i++){
+            let link = allScorecardTags[i].href;
+            let completeLink = "https://www.espncricinfo.com"+link;
+            // console.log(completeLink);
             request(completeLink,cb2);
+            counter++;
         }
+        // console.log("Line 24: ",leaderboard);
     }
 }
 
-function cb2(response,error,html)
-{
-    if (error)
-    {
+function cb2(error,response,html){
+    if(error){
         console.log(error);
-    }
-    else
-    {
-        const dom=new JSDOM(html);
-        const document=dom.window.document;
-
-        let batsmanRows=document.querySelectorAll(".table.batsman tbody tr");
-
-        for(let i=0;i<batsmanRows.length;i++)
-        {
-            let cells=batsmanRows[i].querySelectorAll("td");
-            if(cells.length==8)
-            {
-                let name=cells[0].textContent;                  //from here we will get all the required detials of each batsman 
-                let runs=cells[2].textContent;
-                let balls=cells[3].textContent;
-                let fours=cells[5].textContent;
-                let six=cells[6].textContent;
+    }else{
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        let batsmenRow = document.querySelectorAll(".table.batsman tbody tr");
+        for(let i=0;i<batsmenRow.length;i++){
+            let cells = batsmenRow[i].querySelectorAll("td");
+            if(cells.length == 8){
+                let name = cells[0].textContent;
+                let runs = cells[2].textContent;
+                let balls = cells[3].textContent;
+                let fours = cells[5].textContent;
+                let sixes = cells[6].textContent;
+                // console.log("Name : ",name,"Runs : ",runs,"Balls : ",balls,"Fours : ",fours,"Sixes : ",sixes);
+                processPlayer(name,runs,balls,fours,sixes);
             }
         }
+        counter--;
+        if(counter == 0){
+            console.log(leaderboard);
+            let data = JSON.stringify(leaderboard);
+            fs.writeFileSync('BatsmenStats.json',data);
 
 
+        }
     }
 }
 
@@ -83,11 +78,13 @@ function processPlayer(name,runs,balls,fours,sixes){
     // code coming here means we did not find our player inside leaderboard
     let obj = {
         Name:name,
-        Runs:runs,
         Innings:1,
+        Runs:runs,
         Balls:balls,
         Fours:fours,
         Sixes:sixes
     }
     leaderboard.push(obj);
 }
+
+// console.log("Line 80: ",leaderboard);
