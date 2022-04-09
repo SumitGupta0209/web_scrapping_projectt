0dc8cf528f142deea4429a6ed63b608021cd922f
Sumit Gupta <gsumit232@gmai.com>
2022-04-09 22:49:06 +0530

    code completed

--- leader_board.js
+++ leader_board.js
@@ -11,34 +11,41 @@ let counter = 0;
 
 request(link, cb);
 
-function cb(error, response, html) {
-    if (error) {
+function cb(error, response, html) 
+{
+    if (error) 
+    {
         console.log(error);
-    } else {
+    } else 
+    {
         const dom = new JSDOM(html);
         const document = dom.window.document;
         let allScorecardTags = document.querySelectorAll('.ds-border-b.ds-border-line');
-        // console.log(allScorecardTags.length);
-        for (let i = 0; i < 60; i++) {
+        for (let i = 0; i < 60; i++) 
+        {
             let anchorTagAll = allScorecardTags[i].querySelectorAll("a");
             let link = anchorTagAll[2].href;
             let completeLink = "https://www.espncricinfo.com" + link;
-            // console.log(completeLink);
+            
             request(completeLink, cb2);
             counter++;
         }
-        // console.log("Line 24: ",leaderboard);
+        
     }
 }
 
-function cb2(error, response, html) {
-    if (error) {
+function cb2(error, response, html)
+ {
+    if (error) 
+    {
         console.log(error);
-    } else {
+    } else
+     {
         const dom = new JSDOM(html);
         const document = dom.window.document;
         let batsmenRow = document.querySelectorAll('tbody [class="ds-border-b ds-border-line ds-text-tight-s"]');
-        for (let i = 0; i < batsmenRow.length; i++) {
+        for (let i = 0; i < batsmenRow.length; i++) 
+        {
             let cells = batsmenRow[i].querySelectorAll("td");
             if (cells.length == 8) {
                 let name = cells[0].textContent;
@@ -46,12 +53,13 @@ function cb2(error, response, html) {
                 let balls = cells[3].textContent;
                 let fours = cells[5].textContent;
                 let sixes = cells[6].textContent;
-                // console.log("Name : ",name,"Runs : ",runs,"Balls : ",balls,"Fours : ",fours,"Sixes : ",sixes);
+               
                 processPlayer(name, runs, balls, fours, sixes);
             }
         }
         counter--;
-        if (counter == 0) {
+        if (counter == 0)
+         {
             console.log(leaderboard);
             let data = JSON.stringify(leaderboard);
             fs.writeFileSync('BatsmenStats.json', data);
@@ -86,10 +94,12 @@ function processPlayer(name, runs, balls, fours, sixes) {
     balls = Number(balls);
     fours = Number(fours);
     sixes = Number(sixes);
-    for (let i = 0; i < leaderboard.length; i++) {
+    for (let i = 0; i < leaderboard.length; i++)
+     {
         let playerObj = leaderboard[i];
-        if (playerObj.Name == name) {
-            //will do some work here
+        if (playerObj.Name == name)
+         {
+           
             playerObj.Runs += runs;
             playerObj.Innings += 1;
             playerObj.Balls += balls;
@@ -98,7 +108,7 @@ function processPlayer(name, runs, balls, fours, sixes) {
             return;
         }
     }
-    // code coming here means we did not find our player inside leaderboard
+   
     let obj = {
         Name: name,
         Innings: 1,
@@ -109,5 +119,3 @@ function processPlayer(name, runs, balls, fours, sixes) {
     }
     leaderboard.push(obj);
 }
-
-// console.log("Line 80: ",leaderboard);
